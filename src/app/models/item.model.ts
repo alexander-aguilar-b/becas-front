41da49b1756e2c62f618e069219fe3da9513c951
Edgar Alexander Aguilar <ea.aguilar@uniandes.edu.co>
2017-08-11 17:11:06 -0500

    Creacion de Formulario

---
+++ src/app/models/item.model.ts
@@ -0,0 +1,33 @@
+import {Respuesta} from "./respuesta.model";
+/**
+ * Created by edgaguil on 10/08/2017.
+ */
+export class Item
+{
+  public id : number;
+  public descripcion : string;
+  public idTipoControl : number;
+  public valor : string;
+  public activo : boolean;
+  public listaRespuestas? : Respuesta[];
+  public requerido : boolean;
+  public tamanoMaximo : number;
+
+  constructor(){}
+
+
+
+}
+
+export interface IItem
+{
+  id : number,
+  descripcion : string,
+  idTipoControl : number,
+  valor : string;
+  activo : boolean;
+  listaRespuestas? : Respuesta[];
+
+}
+
+
